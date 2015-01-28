d720eb521ec6ca6a3b89b63c7e947245c713670c
d.efimov <d.efimov@simcase.ru>
2015-01-28 21:37:00 +0300

    Added generat CRUD User.

---
+++ TrainingCentersCRM/TrainingCentersCRM/Controllers/UserController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TrainingCentersCRM.Models;
+
+namespace TrainingCentersCRM.Controllers
+{
+    public class UserController : Controller
+    {
+        private TrainingCentersDBEntities db = new TrainingCentersDBEntities();
+
+        // GET: /User/
+        public ActionResult Index()
+        {
+            var aspnetusers = db.AspNetUsers.Include(a => a.User);
+            return View(aspnetusers.ToList());
+        }
+
+        // GET: /User/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspnetuser = db.AspNetUsers.Find(id);
+            if (aspnetuser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspnetuser);
+        }
+
+        // GET: /User/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "Id", "LastName");
+            return View();
+        }
+
+        // POST: /User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="UserId,Id,UserName,PasswordHash,SecurityStamp")] AspNetUser aspnetuser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AspNetUsers.Add(aspnetuser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "Id", "LastName", aspnetuser.UserId);
+            return View(aspnetuser);
+        }
+
+        // GET: /User/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspnetuser = db.AspNetUsers.Find(id);
+            if (aspnetuser == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "LastName", aspnetuser.UserId);
+            return View(aspnetuser);
+        }
+
+        // POST: /User/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="UserId,Id,UserName,PasswordHash,SecurityStamp")] AspNetUser aspnetuser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspnetuser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "LastName", aspnetuser.UserId);
+            return View(aspnetuser);
+        }
+
+        // GET: /User/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetUser aspnetuser = db.AspNetUsers.Find(id);
+            if (aspnetuser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspnetuser);
+        }
+
+        // POST: /User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AspNetUser aspnetuser = db.AspNetUsers.Find(id);
+            db.AspNetUsers.Remove(aspnetuser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
