a4f3116fe78d1a56845f61293530f3829f957833
d.efimov <d.efimov@simcase.ru>
2015-01-28 20:05:16 +0300

    Created model for Feedback page.

---
+++ TrainingCentersCRM/TrainingCentersCRM/Models/Feedback.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrainingCentersCRM.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Имя")]
+        [StringLength(250, ErrorMessage = "Заголовок не может быть больше 250 символов.")]
+        public string Name { get; set; }
+
+        [Display(Name = "E-mail")]
+        [StringLength(250, ErrorMessage = "Поле не может быть больше 250 символов.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Сообщение")]      
+        public string Message { get; set; }
+      
+        public DateTime Date { get; set; }
+    }
+}
