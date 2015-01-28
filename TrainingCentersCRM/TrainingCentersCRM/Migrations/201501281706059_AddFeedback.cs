aa610e821362495eb1dbc90c10a271df22dd8e69
d.efimov <d.efimov@simcase.ru>
2015-01-28 20:07:20 +0300

    Added migration AddFeedback for created table Feedback in database.

---
+++ TrainingCentersCRM/TrainingCentersCRM/Migrations/201501281706059_AddFeedback.cs
@@ -0,0 +1,29 @@
+namespace TrainingCentersCRM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddFeedback : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Feedbacks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 250),
+                        Email = c.String(maxLength: 250),
+                        Message = c.String(),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Feedbacks");
+        }
+    }
+}
