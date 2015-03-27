ae437fc9e02153dbff1bec3b608110de9f3bb030
Dmitry Efimov <d.efimov@simcase.ru>
2015-03-27 19:41:55 +0300

    Added Teacher CRUD.

---
+++ TrainingCentersCRM/TrainingCentersCRM/Controllers/TeacherController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TrainingCentersCRM.Models;
+
+namespace TrainingCentersCRM.Controllers
+{
+    public class TeacherController : RoutingTrainingCenterController
+    {
+        private TrainingCentersDBEntities db = new TrainingCentersDBEntities();
+
+        // GET: /Teacher/
+        public ActionResult Index()
+        {
+            return View(db.Teachers.ToList());
+        }
+
+        // GET: /Teacher/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // GET: /Teacher/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Teacher/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,LastName,FirstName,Patronymic,Email,Description,Phone")] Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(teacher);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(teacher);
+        }
+
+        // GET: /Teacher/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // POST: /Teacher/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,LastName,FirstName,Patronymic,Email,Description,Phone")] Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(teacher).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(teacher);
+        }
+
+        // GET: /Teacher/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // POST: /Teacher/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Teacher teacher = db.Teachers.Find(id);
+            db.Users.Remove(teacher);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
